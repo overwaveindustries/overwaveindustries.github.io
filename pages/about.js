28b0c7e22624346e9b245195372d01178a839bba
Krish Dholakiya <krishna.dholakiya@gmail.com>
2021-12-29 20:00:00 -0500

    init

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.aboutContainer}>
+        <h1>yo!</h1>
+    </div>
+  )
+}
